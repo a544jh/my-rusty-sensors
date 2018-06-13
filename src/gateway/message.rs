c2c09487c49829971c9736c35b0686984a833b43
a544jh <axel.wikstrom@gmail.com>
2018-06-13 23:15:35 +0300

    finish proper encoding and decoding

--- src/gateway/message.rs
+++ src/gateway/message.rs
@@ -1,5 +1,6 @@
 use num::ToPrimitive;
 use num::FromPrimitive;
+use std::fmt;
 
 // https://www.mysensors.org/download/serial_api_20
 
@@ -19,6 +20,28 @@ pub enum PayloadType {
     Str(String),
 }
 
+impl fmt::Display for PayloadType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            PayloadType::Int(i) => write!(f,"{}",i),
+            PayloadType::Float(fl) => write!(f,"{}",fl), //TODO: may need fxing
+            PayloadType::Str(s) => write!(f,"{}",s),
+        }
+    }
+}
+
+impl PayloadType {
+    pub fn decode(s: &str) -> PayloadType {
+        if let Ok(u) = s.parse::<u32>() {
+            return PayloadType::Int(u);
+        }
+        if let Ok(f) = s.parse::<f32>() {
+            return PayloadType::Float(f);
+        }
+        PayloadType::Str(s.to_string())
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub enum Command {
     Presentation(Sensor),
@@ -30,30 +53,28 @@ pub enum Command {
 
 impl Command {
     pub fn decode(ints: (u32, u32)) -> Command {
-        let (cmd, cmd_type) = ints;
+        let (cmd, typ) = ints;
 
         match cmd {
             0 => {
-                let typ = FromPrimitive::from_u32(cmd_type).unwrap();
+                let typ = FromPrimitive::from_u32(typ).unwrap();
                 Command::Presentation(typ)
             }
             1 => {
-                let typ = FromPrimitive::from_u32(cmd_type).unwrap();
+                let typ = FromPrimitive::from_u32(typ).unwrap();
                 Command::Set(typ)
             }
             2 => {
-                let typ = FromPrimitive::from_u32(cmd_type).unwrap();
+                let typ = FromPrimitive::from_u32(typ).unwrap();
                 Command::Req(typ)
             }
             3 => {
-                let typ = FromPrimitive::from_u32(cmd_type).unwrap();
+                let typ = FromPrimitive::from_u32(typ).unwrap();
                 Command::Internal(typ)
             }
             4 => Command::Stream,
             _ => panic!("Invalid message type")
         }
-
-        // Command::Presentation(SensorType::Door)
     }
 
     pub fn encode(&self) -> (u32, u32) {
@@ -85,6 +106,42 @@ pub enum Sensor {
     Motion,
     Smoke,
     Binary,
+    Dimmer,
+    Cover,
+    Temperature,
+    Humidity,
+    Barometer,
+    Wind,
+    Rain,
+    UV,
+    Weight,
+    Power,
+    Heater,
+    Distance,
+    LightLevel,
+    ArduinoNode,
+    ArduinoRepeaterNode,
+    Lock,
+    IR,
+    Water,
+    AirQuality,
+    Custom,
+    Dust,
+    SceneController,
+    RgbLight,
+    RgbwLight,
+    ColorSensor,
+    HVAC,
+    Multimeter,
+    Sprinkler,
+    WaterLeak,
+    Sound,
+    Vibration,
+    Moisture,
+    Info,
+    Gas,
+    GPS,
+    WaterQuality
 }
 
 #[derive(Debug, PartialEq, ToPrimitive, FromPrimitive)]
@@ -93,6 +150,59 @@ pub enum Payload {
     Humidity,
     Status,
     Percentage,
+    Pressure,
+    WeatherForecast,
+    RainAmount,
+    RainRate,
+    WindSpeed,
+    Gust,
+    Direction,
+    UV,
+    Weight,
+    Distance,
+    Impedance,
+    Armed,
+    Tripped,
+    Watt,
+    KWH,
+    SceneOn,
+    SceneOff,
+    HvacFlowState,
+    HvacSpeed,
+    LightLevel,
+    Var1,
+    Var2,
+    Var3,
+    Var4,
+    Var5,
+    Up,
+    Down,
+    Stop,
+    IrSend,
+    IrReceive,
+    WaterFlow,
+    WaterVolume,
+    LockStatus,
+    Level,
+    Voltage,
+    Current,
+    RGB,
+    RGBW,
+    ID,
+    UnitPrefix,
+    HvacSetpointCool,
+    HvacSetpointHeat,
+    HvacFlowMode,
+    Text,
+    Custom,
+    Position,
+    IrRecord,
+    WaterPh,
+    WaterOrp,
+    WaterEc,
+    ReactivePower,
+    ApparentPower,
+    PoweFactor,
 }
 
 #[derive(Debug, PartialEq, ToPrimitive, FromPrimitive)]
@@ -101,4 +211,29 @@ pub enum Internal {
     Time,
     Version,
     IdRequest,
+    IdResponse,
+    InclusionMode,
+    Config,
+    FindParent,
+    FindParentResponse,
+    LogMessage,
+    Children,
+    SketchName,
+    SketchVersion,
+    Reboot,
+    GatewayReady,
+    SigningPresentation,
+    NonceRequest,
+    NonceResponce,
+    HeartbeatRequest,
+    Presentation,
+    DiscoverRequest,
+    DiscoverResponse,
+    HeartbeatResponse,
+    Locked,
+    Ping,
+    Pong,
+    RegistrationRequest,
+    RegistrationResponse,
+    Debug,
 }
--- src/gateway/serial.rs
+++ src/gateway/serial.rs
@@ -3,20 +3,37 @@ use super::message::Sensor::*;
 // use num::*;
 
 pub fn encode(msg: &message::Message) -> String {
-    //let i :u32 = *msg as u32;
     let command = &msg.command;
-    let (i, _) = command.encode();
-    i.to_string()
+    let (cmd, typ) = command.encode();
+    let ack = match msg.ack {
+        true => "1",
+        false => "0",
+    };
+    format!("{};{};{};{};{};{}\n",
+        msg.node_id,
+        msg.child_sensor_id,
+        cmd,
+        ack,
+        typ,
+        msg.payload,
+    )
 }
 
 fn decode(msg_str: &str) -> message::Message {
-    let i: u32 = msg_str.parse().unwrap();
+    let mut it = msg_str.split(";");
+    let node_id: u32 = it.next().unwrap().parse().unwrap();
+    let child_sensor_id: u32 = it.next().unwrap().parse().unwrap();
+    let cmd: u32 = it.next().unwrap().parse().unwrap();
+    let ack: u32 = it.next().unwrap().parse().unwrap();
+    let typ: u32 = it.next().unwrap().parse().unwrap();
+    let pl = it.next().unwrap().trim();
+
     message::Message {
-        node_id: 0,
-        child_sensor_id: 0,
-        command: message::Command::decode((i, 0)),
-        ack: false,
-        payload: message::PayloadType::Int(0),
+        node_id,
+        child_sensor_id,
+        command: message::Command::decode((cmd, typ)),
+        ack: if ack != 0 {true} else {false},
+        payload: message::PayloadType::decode(pl),
     }
 }
 
@@ -35,12 +52,12 @@ mod tests {
             payload: message::PayloadType::Int(0),
         };
         let msg_str = encode(&msg);
-        assert_eq!(msg_str, "0");
+        assert_eq!(msg_str, "0;0;0;0;0;0\n");
     }
 
     #[test]
     fn can_decode() {
-        let msg_str = "0";
+        let msg_str = "0;0;0;0;0;0\n";
         let msg = decode(&msg_str);
         let expected = message::Message {
             node_id: 0,
